66d6f06e3f12b45f2317098798ac29316aef9527
mariagarciaflores <grissgarcia74@gmail.com>
2018-08-14 20:26:56 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss--routing']
+})
+export class AppComponent {
+  title = 'check-in-web';
+}
