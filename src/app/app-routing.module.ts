7c645fe5e9719e0706f8480cc1c42a9fa0db4077
mariagarciaflores <grissgarcia74@gmail.com>
2018-08-14 22:35:45 -0400

    first layout with semantic and new folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
+import { CheckInComponent } from './check-in/check-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/check-in',
+        pathMatch: 'full'
+      },
+      {
+        path: 'check-in',
+        component: CheckInComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckInComponent } from './check-in/check-in.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckInComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
+    SuiModule,
     AppRoutingModule
   ],
   providers: [],
