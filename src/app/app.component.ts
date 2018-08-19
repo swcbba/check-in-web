386acccdc99652744c0f0fc920529f14ac3a4fe7
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-08-19 14:10:11 -0400

    Check for app updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'sw-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'check-in-web';
+  showUpdateSegment: boolean;
+
+  constructor(private updates: SwUpdate) {
+    this.showUpdateSegment = false;
+    this.updates.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      this.showUpdateSegment = true;
+    });
+    this.updates.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+  }
+
+  updateApp(): void {
+    document.location.reload();
+  }
+
+  hideUpdateSegment(): void {
+    this.showUpdateSegment = false;
+  }
 }
