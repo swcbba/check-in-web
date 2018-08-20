f29f126c3b3c1c3a0c04bd506b99896848358fef
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-08-20 14:12:09 -0400

    Initial template for adding new volunteers created

--- src/app/volunteers/volunteers.component.ts
+++ src/app/volunteers/volunteers.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { VolunteersService } from './volunteers.service';
 import { IVolunteer } from './i-volunteer';
+
+declare const $: any;
 
 @Component({
   selector: 'sw-volunteers',
   templateUrl: './volunteers.component.html',
   styleUrls: ['./volunteers.component.scss']
 })
-export class VolunteersComponent {
+export class VolunteersComponent implements OnInit {
   volunteers$: Observable<Array<IVolunteer>>;
 
   constructor(private volunteersService: VolunteersService) {
     this.volunteers$ = this.volunteersService.getVolunteers();
+  }
+
+  ngOnInit(): void {
+    $('select.dropdown').dropdown();
+  }
+
+  openAddVolunteersModal(): void {
+    $('.ui.modal').modal('show');
   }
 }
