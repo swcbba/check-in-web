144f8411eb613093e198ad250df42d4ed66c1f4f
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-09-13 17:19:49 -0400

    Button and modal to add new meeting were added

--- src/app/meetings/meetings.component.ts
+++ src/app/meetings/meetings.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { MeetingsService } from './meetings.service';
 import { Meeting } from './meeting';
+
+declare const $: any;
 
 @Component({
   selector: 'sw-meetings',
   templateUrl: './meetings.component.html',
   styleUrls: ['./meetings.component.scss']
 })
-export class MeetingsComponent {
+export class MeetingsComponent implements OnInit {
   meetings$: Observable<Array<Meeting>>;
 
   constructor(private meetingsService: MeetingsService) {
     this.meetings$ = this.meetingsService.getMeetings();
+  }
+
+  ngOnInit(): void {
+    $('#example1').calendar();
+  }
+
+  showEditMeetingModal(): void {
+    $('#edit-meeting-modal').modal('show');
   }
 }
--- src/app/meetings/meetings.module.ts
+++ src/app/meetings/meetings.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MeetingsRoutingModule } from './meetings-routing.module';
 import { MeetingsComponent } from './meetings.component';
 import { CheckInComponent } from './check-in/check-in.component';
 
 @NgModule({
-  imports: [CommonModule, MeetingsRoutingModule],
+  imports: [CommonModule, FormsModule, MeetingsRoutingModule],
   declarations: [MeetingsComponent, CheckInComponent]
 })
 export class MeetingsModule {}
